918f4c82d2105284b3a8759e11bc1630dd0eb85f
Zyun Lam <zyunlam@gmail.com>
2017-11-29 09:43:59 -0600

    Work on manual`

--- src/ConquerSpace/start/gui/Manual.java
+++ src/ConquerSpace/start/gui/Manual.java
@@ -2,31 +2,61 @@ package ConquerSpace.start.gui;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.DefaultListModel;
 import java.util.logging.Logger;
-import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import ConquerSpace.util.CQSPLogger;
 
-
-public class Manual extends JFrame {
+public class Manual extends JFrame implements ListSelectionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2589166556749207075L;
 	private static Manual current;
 	private static final Logger LOGGER = CQSPLogger.getLogger(Manual.class.getName());
-	
-	
-	private Manual () {
-		setSize(300, 200);
-		setTitle("Manual");
-		DefaultListModel model = new DefaultListModel();
-		model.addElement("Hello");
-		JList list = new JList(model);
-		add(list);
+
+	private JList<String> list;
+	private Manual() {
+		try {
+			setSize(300, 200);
+			
+			setTitle("Manual");
+			DefaultListModel<String> model = new DefaultListModel<>();
+			// Open property file
+			Properties prop = new Properties();
+
+			prop.load(new FileInputStream(System.getProperty("user.dir") + "/assets/manuals/manlist.properties"));
+			
+			for (Object val : prop.values()) {
+				model.addElement((String) val);
+			}
+			list = new JList<>(model);
+			list.addListSelectionListener(this);
+			add(list);
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
-	
-	public static Manual getInstance () {
+
+	public static Manual getInstance() {
 		if (current == null) {
 			current = new Manual();
 		}
 		return current;
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		String val = list.getSelectedValue();
+		System.out.println(val);
 	}
 }
