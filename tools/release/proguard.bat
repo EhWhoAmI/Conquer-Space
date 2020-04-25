d3964cff776b5175fea071e94ea18606de4bca01
ehwhoami <zyunlam@gmail.com>
2020-04-25 09:14:08 -0500

    Work on more build stuff.

---
+++ tools/release/proguard.bat
@@ -0,0 +1,3 @@
+@echo off
+REM This is just a wrapper for the python file, as changing the path in python is insane. Don't do that.
+../proguard/bin/proguard @../../proguard.conf -verbose
---
+++ tools/release/release.py
@@ -0,0 +1,24 @@
+import os
+import zipfile
+import shutil
+
+cqsp_dir = '../../'
+build_dir = cqsp_dir + 'dist/'
+zip_dir = cqsp_dir + 'Conquer-Space/'
+zip_name = 'Conquer_Space'
+if __name__ == '__main__':
+    if os.path.isdir(build_dir):
+        os.system('proguard')
+        
+        # Rename files
+        # Remove og conquer space file
+        os.remove(build_dir + 'Conquer-Space.jar')
+        os.rename(build_dir + 'Conquer-Space-compressed.jar', build_dir + 'Conquer-Space.jar')
+        os.rename(build_dir, zip_dir)
+        print('Zipping...')
+        shutil.make_archive(cqsp_dir + zip_name, 'zip', zip_dir)
+        # Rename back so you don't lose it
+        os.rename(zip_dir, build_dir)
+        print('Done!')
+    else:
+        print('you probably need to recompile.')
