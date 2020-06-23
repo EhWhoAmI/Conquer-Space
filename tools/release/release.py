dfa8e7268bd26aeef8f580f4185005bba355d947
EhWhoAmI <zyunlam@gmail.com>
2020-06-23 00:45:02 -0500

    Update release.py
    No more compression

--- tools/release/release.py
+++ tools/release/release.py
@@ -8,14 +8,10 @@ zip_dir = cqsp_dir + 'Conquer-Space/'
 zip_name = 'Conquer_Space'
 if __name__ == '__main__':
     if os.path.isdir(build_dir):
-        if 'win' in platform.system().lower():
-             os.system('proguard.bat')
-        elif 'nux' in platform.system().lower():
-             os.system('./proguard.sh')
         # Rename files
         # Remove og conquer space file
         os.remove(build_dir + 'Conquer-Space.jar')
-        os.rename(build_dir + 'Conquer-Space-compressed.jar', build_dir + 'Conquer-Space.jar')
+        #os.rename(build_dir + 'Conquer-Space-compressed.jar', build_dir + 'Conquer-Space.jar')
         os.rename(build_dir, zip_dir)
         print('Zipping...')
         shutil.make_archive(cqsp_dir + zip_name, 'zip', zip_dir)
